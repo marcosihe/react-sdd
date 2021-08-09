85642425527b4fafcb0d8f610c4778162e4ec2a7
marcosihe <marcosherrera.ib@gmail.com>
2021-08-09 12:03:15 -0300

    Agregar solicitud get para obtener clientes en el componente Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Debtors from "./pages/Debtors";
 import Contact from "./pages/Contact";
 import Error404 from "./pages/Error404";
 import AddCustomer from "./AddCustomer";
+import { apiRequest } from "../helpers/apiRequest";
 
 const Main = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    apiRequest(setCustomers);
+  }, [])
+  console.log(customers);
+
   return (
     <main>
       <Switch>
@@ -14,7 +22,7 @@ const Main = () => {
           <Home />
         </Route>
         <Route exact path="/deudores">
-          <Debtors />
+          <Debtors customers={customers}/>
         </Route>
         <Route exact path="/contacto">
           <Contact />
---
+++ src/helpers/apiRequest.js
@@ -0,0 +1,16 @@
+const responseStatus = 200;
+const URL = process.env.REACT_APP_API_URL;
+
+const apiRequest = async(setState) => {
+    try {
+      const response = await fetch(URL);
+      console.log(response);
+      if (response.status === responseStatus) {
+        setState( await response.json() );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export { apiRequest }
