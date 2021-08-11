cfd599099e7349f46094d05472b79a0a5bb9ec56
marcosihe <marcosherrera.ib@gmail.com>
2021-08-11 10:09:04 -0300

    Agregar maquetado del componente CurrentDebt con su formulario de pago

---
+++ src/components/CurrentDebt.jsx
@@ -0,0 +1,36 @@
+import { faHandPointLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import styles from "../css/CurrentDebt.module.css";
+
+const CurrentDebt = ({ currentDebt }) => {
+  currentDebt = 1250; //monto de prueba para maquetado
+  return (
+    <section className={styles.currentDebtContainer}>
+      <div className={styles.amountContainer}>
+        <div className={styles.debtAmount}>${currentDebt}</div>
+        <FontAwesomeIcon
+          icon={faHandPointLeft}
+          className={styles.handPointLeftIcon}
+        />
+        <p className={styles.currentDebtText}>Deuda Actual</p>
+      </div>
+      <div className={styles.currentDebtForm}>
+        <div className={styles.amountToPayLabel}><p>(Ingrese la cantidad a pagar)</p></div>
+        <Form className={styles.payForm}>
+          <Form.Group>
+            <Form.Control type="number" placeholder={0} />
+          </Form.Group>
+          <Form.Group>
+            <Button type="submit" variant="success" className={styles.payButton}>
+              Pagar
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+    </section>
+  );
+};
+
+export default CurrentDebt;
--- src/components/CustomerDetails.jsx
+++ src/components/CustomerDetails.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Container } from "react-bootstrap";
 import styles from "../css/CustomerDetails.module.css";
+import CurrentDebt from "./CurrentDebt";
 import CustomerData from "./CustomerData";
 
 const CustomerDetails = () => {
@@ -15,11 +16,7 @@ const CustomerDetails = () => {
         />
         <CustomerData className={styles.customerData} />
       </section>
-      <section>
-        <div>$1250</div>
-        <div>Saldar parte de la deuda</div>
-        <div>Saldar deuda completa</div>
-      </section>
+      <CurrentDebt/>
       <section>
         <h2>Historial de compras</h2>
         <p>Monto de la compra: $650</p>
